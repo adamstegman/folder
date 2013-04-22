6f0f38a7478b0b153a5a8a28cbc4782f9159d558
Adam Stegman <adam.stegman@cerner.com>
2013-04-22 16:33:03 -0500

    New Rails application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ImageHost::Application.config.secret_token = '9504b82475767e37b627850b2eece2b29107e744d508081151d778cb213bc9d0075aa176c0393eeb2fc2d8f44d2d0ddfc6a773db69f5957851bd6f1b81dcf912'
