43b6baea9809f97c4ee071e32d492038cbf8fc89
Adam Stegman <adam.stegman@cerner.com>
2013-05-09 14:45:21 -0500

    Use application_ruby master

--- config/chef/cookbooks/folder/metadata.rb
+++ config/chef/cookbooks/folder/metadata.rb
@@ -5,4 +5,4 @@ description      'Installs the folder application and runs it using Unicorn.'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '1.0.0'
 
-depends 'application_ruby', '1.1.0'
+depends 'application_ruby', '1.1.3'
